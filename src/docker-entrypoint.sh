b56554643936b0a81b41a13aa32fc045a958a7d8
Lam Huynh <thanhlam0290@gmail.com>
2023-09-15 22:43:46 +0700

    Update docker-entrypoint.sh

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -13,6 +13,22 @@ adduser \
 	-u $UID \
 	$FTP_USER
 
+config_file="/etc/vsftpd.conf"
+
+config_line_addr="pasv_address="
+new_config_line_addr="pasv_address=$PASV_ADDRESS"
+sed -i "s/$config_line_addr.*/$new_config_line_addr/" "$config_file"
+
+config_line_min="pasv_min_port="
+new_config_line_min="pasv_min_port=$PASV_MIN_PORT"
+sed -i "s/$config_line_min.*/$new_config_line_min/" "$config_file"
+
+config_line_max="pasv_max_port="
+new_config_line_max="pasv_max_port=$PASV_MAX_PORT"
+sed -i "s/$config_line_max.*/$new_config_line_max/" "$config_file"
+
+
+
 mkdir -p /home/$FTP_USER
 chown -R $FTP_USER:$FTP_USER /home/$FTP_USER
 echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd
